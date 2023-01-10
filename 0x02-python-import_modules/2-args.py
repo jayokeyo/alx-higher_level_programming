5501316168acfedfb2575dbd0871cb27463e1806
Julius-Okeyo <jaykopiyo@gmail.com>
2023-01-10 07:53:46 -0800

    Added condition for count == 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
 
-    if count == 1:
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
