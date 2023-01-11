429b1af4bb871cecb8322814f4edd1e9679abd5c
Julius-Okeyo <jaykopiyo@gmail.com>
2023-01-11 15:03:52 -0800

    Added for loop

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@
 # Julius Okeyo <jaykopiyo@gmail.com>
 
 def search_replace(my_list, search, replace):
-    return (list(map(lambda x: x = replace if x == search, my_list)))
+    return (list(map(lambda x: x = replace if x == search else x = my_list[i] for i in range(len(my_list)), my_list)))
