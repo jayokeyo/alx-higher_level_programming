629a942e9e27ee903ac97bf4d4063406cfb78fe7
Julius-Okeyo <jaykopiyo@gmail.com>
2023-01-11 18:32:02 -0800

    Added 11-multiply_list_map.py

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 11-multiply_list_map.py
+# Julius Okeyo <jaykopiyo@gmail.com>
+
+def multiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: number * x, my_list)))
