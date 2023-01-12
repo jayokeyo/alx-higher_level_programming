173092b3364a4fc2d2ed28bb40d4412604d19dbe
Julius-Okeyo <jaykopiyo@gmail.com>
2023-01-11 17:55:33 -0800

    Added 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Julius Okeyo <jaykopiyo@gmail.com>
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
