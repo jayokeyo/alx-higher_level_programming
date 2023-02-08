87c547e648214ac7a070d0cf29b07692bd67c899
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-08 12:08:36 -0800

    Added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Defines class BaseGeometry with method area()
+'''
+
+class BaseGeometry:
+    '''
+    Class BaseGeometry with method area()
+    '''
+
+    def area(self):
+        '''
+        Raises exception if called
+        '''
+
+        raise Exception("area() is not implemented")
