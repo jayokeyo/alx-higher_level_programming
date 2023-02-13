a6bc576ffdc03d8f9ea7029dca3bbc07c0a8680f
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-13 01:04:46 -0800

    Updated rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,48 +25,72 @@ class Rectangle(Base):
         Gets width
         '''
         return self.__width
+
     @width.setter
     def width(self, width):
         '''
         Sets width
         '''
+        if (width is not int):
+            raise TypeError("width must be an integer")
+        if (width <= 0):
+            raise ValueError("width must be > 0")
         self.__width = width
+
     @property
     def height(self):
         '''
         Gets height
         '''
         return self.__height
+
     @height.setter
     def height(self, height):
         '''
         Sets height
         '''
+        if (height is not int):
+            raise TypeError("height must be an integer")
+        if (height <= 0):
+            raise ValueError("height must be > 0")
         self.__height = height
+
     @property
     def x(self):
         '''
         Gets x
         '''
         return self.__x
+
     @x.setter
     def x(self, x):
         '''
         Sets x
         '''
+        if (x is not int):
+            raise TypeError("x must be an integer")
+        if (x < 0):
+            raise ValueError("x must be >= 0")
         self.__x = x
+
     @property
     def y(self):
         '''
         Gets y
         '''
         return self.__y
+
     @y.setter
     def y(self, y):
         '''
         Sets y
         '''
+        if (y is not int):
+            raise TypeError("y must be an integer")
+        if (y < 0):
+            raise ValueError("y must be >= 0")
         self.__y = y
+
     def __init__(self, width, height, x=0, y=0, id=None):
         '''
         Class constructor for class Rectangle
