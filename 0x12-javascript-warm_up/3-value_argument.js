cf9c9ea71c8829a87e2cf028f47b9828f5840bd3
Julius-Okeyo <jaykopiyo@gmail.com>
2023-04-12 15:39:49 -0700

    Added comment

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
+/*
+ * Use console.log to print first argument passed to function
+ */
 let firstArgument = process.argv[2];
 console.log(firstArgument === undefined ? 'No argument' : firstArgument);
