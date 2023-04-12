317227362efbcaa7af58484619d89527e6e924ce
Julius-Okeyo <jaykopiyo@gmail.com>
2023-04-12 16:09:59 -0700

    Changed logged message

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 /*
  * Use console.log to print out two arguments passed to function
  */
-console.log(process.argv[2]+' '+process.argv[3]);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
