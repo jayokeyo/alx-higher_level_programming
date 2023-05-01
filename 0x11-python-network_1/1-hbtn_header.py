cdfe5db48c43324189a9bebb9736ba76c67afa7b
Julius-Okeyo <jaykopiyo@gmail.com>
2023-05-01 04:05:04 -0700

    Limiting importation

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ the header of the response using the packages urllib and sys
 '''
 import urllib.request
 import sys
-
-url = sys.argv[1]
-with urllib.request.urlopen(url) as  response:
-    print(dict(response.headers).get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as  response:
+        print(dict(response.headers).get("X-Request-Id"))
