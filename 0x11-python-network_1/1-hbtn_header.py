3645732694d54cf62fe8bcdb1290b65fe172a3e5
Julius-Okeyo <jaykopiyo@gmail.com>
2023-05-01 04:08:47 -0700

    Trying to fix pycodestyle error

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,9 @@ the header of the response using the packages urllib and sys
 '''
 import urllib.request
 import sys
+
 if __name__ == "__main__":
     url = sys.argv[1]
+
     with urllib.request.urlopen(url) as  response:
         print(dict(response.headers).get("X-Request-Id"))
