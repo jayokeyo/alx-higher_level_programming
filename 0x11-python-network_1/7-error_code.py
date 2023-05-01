a357443bb08c8f3990502091fd6dbfad16c9d774
Julius-Okeyo <jaykopiyo@gmail.com>
2023-05-01 05:42:24 -0700

    Added 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     response = requests.get(url)
     status_code = response.status_code
     if (status_code >= 400):
-        print("Error code: ", status_code)
+        print("Error code: {}".format(status_code))
     else:
         print(response.text)
