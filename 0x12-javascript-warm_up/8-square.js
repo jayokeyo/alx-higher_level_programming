74be780ba4f7e3451bbd1f1147700b497585c10e
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-14 15:02:21 +0000

    Added 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 let row = '';
-const size = process.argv[2];
-for (i = 0; i < size; i++) {
-  for (j = 0; j < size; j++) {
-    row += 'X';
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+    row = '';
   }
-  console.log(row);
-  row = '';
 }
