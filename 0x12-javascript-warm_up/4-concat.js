81cc5b689a6b5dab38f5d872a1f6d5440905a56a
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-14 13:22:28 +0000

    Updated 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log([process.argv[2] , 'is' , process.argv[3]].join(' '))
+let arg1 = process.argv[2];
+let arg2 = process.argv[3];
+if (arg1 && arg2) {
+  console.log([arg1 , 'is' , arg2].join(' '));
+} else if (arg1) {
+  console.log([arg1, 'is', 'undefined'].join(' '));
+} else {
+  console.log('undefined is undefined');
+}
