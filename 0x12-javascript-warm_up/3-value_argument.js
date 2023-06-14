2b5765d48bef94cd3bb58c83a059e0cfa0760188
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-14 12:54:51 +0000

    Added 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const argNumber = process.argv.length;
-console.log(argNumber === 2 ? 'No argument' : process.argv[2]);
+console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]);
