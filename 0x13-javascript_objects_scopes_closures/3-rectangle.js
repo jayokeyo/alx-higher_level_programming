edc10cb0af4750f3cdcdc7493f7e34f2877e85c7
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-14 20:12:02 +0000

    Added 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w === 0 || h === 0 || Math.sign(w) === -1 || Math.sign(h) === -1) {
+      this.width;
+      this.height;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+}
