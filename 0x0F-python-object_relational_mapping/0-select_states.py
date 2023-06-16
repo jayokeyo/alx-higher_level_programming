710b85b7a362f2370efb73e0e2083b091be8482c
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-16 12:26:49 -0700

    Trying to resolve PEP8 errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            user=sys.argv[1], 
-            passwd=sys.argv[2], 
-            db=sys.argv[3]
+            user = sys.argv[1], 
+            passwd = sys.argv[2], 
+            db = sys.argv[3]
             )
     cursor = db.cursor()
     query = "SELECT * FROM `states`"
