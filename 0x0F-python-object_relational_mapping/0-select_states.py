2cbf0f1d9b04dc3364a26c0ec8fbb4899f7d463f
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-16 10:32:34 -0700

    Updated 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,14 @@ import MySQLdb
 import sys
 
 if __name__ == __"__main__":
-    db = MqSQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MqSQLdb.connect(
+            host="localhost", 
+            port=3306, 
+            user=sys.argv[1], 
+            passwd=sys.argv[2], 
+            db=sys.argv[3]
+            )
     cursor = db.cursor()
-    query = "SELECT states FROM hbtn_0e_0_usa"
+    query = "SELECT states FROM hbtn_0e_0_usa ORDER BY states ASC"
     cursor.execute(query)
     [print(state) for state in cursor.fetchall()]
