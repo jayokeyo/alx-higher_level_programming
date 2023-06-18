d492577cb5385d5e746333c5bec90843bf6a8f00
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-18 05:07:03 -0700

    Added 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall() if city[2] == sys.argv[4]]
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
