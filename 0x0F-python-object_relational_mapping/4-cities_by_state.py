98543c636880639ba33d90087702d3e04a99687e
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-18 04:46:22 -0700

    Removed unnecessary bracket

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
             FROM `cities` INNER JOIN `states` \
-            ON `cities`.`state_id`=`state`.`id` \
+            ON `cities`.`state_id` = `state`.`id` \
             ORDER BY `cities`.`id`")
-    [print((city) for city in cursor.fetchall())]
+    [print(city) for city in cursor.fetchall()]
