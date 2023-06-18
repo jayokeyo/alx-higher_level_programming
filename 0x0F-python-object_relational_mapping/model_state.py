24bf0d0b2fdbba17d7849dbba9da2fed939c87e4
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-18 05:34:03 -0700

    Made docstring more elaborate

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 class State(Base):
     '''
     class definition of a State
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
     '''
     __tablename__ = "states"
     id = Column(Integer, autoincrement=True, primary_key=True)
